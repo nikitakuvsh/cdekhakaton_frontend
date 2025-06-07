c667c603838b9cac543183c65242b6a32c2ffb72
nikitakuvsh <world_fre@mail.ru>
2025-06-07 12:35:21 +0300

    init project

---
+++ src/components/Monitoring/Monitoring.jsx
@@ -0,0 +1,7 @@
+import './Monitoring.css';
+
+export default function Monitoring(){
+    return (
+        <div className='monitoring__container'></div>
+    );
+}
---
+++ src/components/Reports/Reports.jsx
@@ -0,0 +1,7 @@
+import './Reports.css';
+
+export default function Reports(){
+    return (
+        <div className='reports__container'></div>
+    );
+}
